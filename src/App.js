f12dcd628bcaab42880062d4638d530f13d7e223
clalil <cliljander@gmail.com>
2019-10-16 20:08:25 +0200

    Add component for display results, change app.js accordingly.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import DisplayCooperResult from './Components/DisplayCooperResult'
+import InputFields from './Components/InputFields'
 
 class App extends Component {
   constructor(props) {
@@ -20,20 +21,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div>
-          <label>Distance</label>
-          <input id="distance" onChange={this.onChange.bind(this)}></input>
-        </div>
-
-        <select id="gender" onChange={this.onChange.bind(this)}>
-          <option value="female">Female</option>
-          <option value="male">Male</option>
-        </select>
-
-        <div>
-          <label>Age</label>
-          <input id="age" onChange={this.onChange.bind(this)}></input>
-        </div>
+        <InputFields 
+        inputChangeHandler={this.onChange.bind(this)}
+        />
 
         <DisplayCooperResult
           distance={this.state.distance}
