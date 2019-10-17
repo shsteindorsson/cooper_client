c3953f358944d8e67030d0f2bca53fe39ffd812c
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-17 13:18:01 +0200

    Move onLogin function to a proper place
    Add import statement for Authenticate model

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import DisplayCooperResult from './Components/DisplayCooperResult'
 import InputFields from './Components/InputFields'
 import LoginForm from './Components/LoginForm'
+import { authenticate } from './Modules/Auth'
+
 
 class App extends Component {
   constructor(props) {
@@ -24,14 +26,23 @@ class App extends Component {
     })
   }
 
+  async onLogin(e) {
+    e.preventDefault();
+    let resp = await authenticate(this.state.email, this.state.password)
+    
+    resp.authenticated === true ? this.setState({ authenticated: true }) 
+    : this.setState({ message: resp.message, renderLoginForm: false })
+  }
+
   render() {
     let renderLogin
-
+    let user
+  debugger
     if (this.state.renderLoginForm === true) {
       renderLogin = (
         <LoginForm 
-        loginHandler={this.onLogin.bind(this)}
-        inputChangeHandler={this.onChange.bind(this)}
+          loginHandler={this.onLogin.bind(this)}
+          inputChangeHandler={this.onChange.bind(this)}
         />
       )
     } else {
@@ -43,18 +54,10 @@ class App extends Component {
       )
     }
 
-    async function onLogin(e) {
-      e.preventDefault();
-      let resp = await authenticate(this.state.email, this.state.password)
-      
-      resp.authenticated === true ? this.setState({ authenticated: true }) 
-      : this.setState({ message: resp.message, renderLoginForm: false })
-    }
-
     return (
       <div>
         <InputFields 
-        inputChangeHandler={this.onChange.bind(this)}
+          inputChangeHandler={this.onChange.bind(this)}
         />
 
         <DisplayCooperResult
