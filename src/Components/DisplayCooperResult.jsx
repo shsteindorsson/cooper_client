d582e5e7062f3fd687e80517e03fd36b911eca1d
clalil <cliljander@gmail.com>
2019-10-17 14:37:28 +0200

    Add conditional for save cooper result.

--- src/Components/DisplayCooperResult.jsx
+++ src/Components/DisplayCooperResult.jsx
@@ -7,13 +7,37 @@ class DisplayCooperResult extends Component {
     return CooperCalculator(this.props.distance, this.props.gender, this.props.age);
   }
 
+  async saveCooperData() {
+    const result = this.calculate()
+    try {
+      await saveData(result)
+      this.props.entryHandler()
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   render() {
     let results
+    let saveButton
+
+    if (this.props.authenticated === true) {
+      saveButton = (
+        <>
+          <button id="save-result" 
+          onClick={this.saveCooperData.bind(this)}>
+            Save entry
+          </button>
+        </>
+      )
+    }
+
     if (this.props.age !== '' && this.props.distance !== '') {
       results =
         <div>
           <p>{this.props.age} y/o {this.props.gender} running {this.props.distance} meters.</p> 
           <p>Result: {this.calculate()}</p>
+          {saveButton}
         </div>
     }
     return (
