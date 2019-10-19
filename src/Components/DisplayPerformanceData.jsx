7b3daebe1ea81ff805da73400b4307c48034dc2a
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-19 11:40:51 +0200

    Add method to get users distances and pass to graph

--- src/Components/DisplayPerformanceData.jsx
+++ src/Components/DisplayPerformanceData.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { getData } from '../Modules/PerformanceData'
-import LineChart from './LineChart'
+import { Line } from 'react-chartjs-2';
 
 class DisplayPerformanceData extends Component {
   constructor(props) {
@@ -12,21 +12,39 @@ class DisplayPerformanceData extends Component {
   componentDidMount() {
     this.getPerformanceData()
   }
-
+  
   async getPerformanceData() {
     let result = await getData()
+    debugger
     this.setState({ performanceData: result.data.entries }, () => { 
       this.props.indexUpdated()
     })
+  }
+
+  async getUserDistances() {
+    let result = await getData()
+    let dataEntries = result.data.entries
+    let distances = []
+    dataEntries.forEach(dist => {distances.push(dist.data.message.split(' ')[0])})
+    return distances
   }
   
   render() {
     let dataIndex
 
+    // const getUserDistances = () => {
+    //   let dataPoints = this.state.performanceData
+    //   debugger
+    //   return dataPoints.data.distance
+
+    // }
+
     if (this.props.updateIndex === true) {
       this.getPerformanceData()
+     // this.getUserDistances()
     }
     if (this.state.performanceData != null) {
+      debugger
       dataIndex = (
         <div>
           {this.state.performanceData.map(item => {
@@ -36,9 +54,38 @@ class DisplayPerformanceData extends Component {
       )
     }
 
+    const mydata = {
+      //labels: getLabels(props.entries),
+      //labels: this.getUserDistances(),
+      datasets: [
+        {
+          label: 'My Cooper Challenge Results',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.getUserDistances()//[65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+    }
+
     return (
       <div>
-        <LineChart entries={this.state.performanceData}/>
+        <h2>Line Chart Example</h2>
+        <Line data={mydata} />
         {dataIndex}
       </div>
     )
