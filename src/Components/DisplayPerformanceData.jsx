3c4e87d269883669b5d272d694023c273db1a6ad
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-19 12:57:37 +0200

    Change data visualization to doughnut

--- src/Components/DisplayPerformanceData.jsx
+++ src/Components/DisplayPerformanceData.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { getData } from '../Modules/PerformanceData'
-import { Line } from 'react-chartjs-2'
+import {Doughnut} from 'react-chartjs-2'
 
 class DisplayPerformanceData extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      performanceData: null,
-      distanceData: null
+      performanceData: null
+     // distanceData: null
     }
   }
 
@@ -35,7 +35,7 @@ class DisplayPerformanceData extends Component {
   
   render() {
     let dataIndex
-    let distanceIndex
+    //let distanceIndex
 
     if (this.props.updateIndex === true) {
       this.getPerformanceData()
@@ -49,51 +49,64 @@ class DisplayPerformanceData extends Component {
           })}
         </div>
       )
-    }
-    if (this.state.distanceData != null) {
-      distanceIndex = (
-        <div>
-          {this.state.performanceData.map(item => {
-            return <div key={item.id}>{item.data.distance}</div>
-          })}
-        </div>
-      )
+      debugger
     }
 
-    const mydata = {
-      //labels: getLabels(props.entries),
-      //labels: this.getUserDistances(),
-      datasets: [
-        {
-          label: 'My Cooper Challenge Results',
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          // data: this.getUserDistances()//[65, 59, 80, 81, 56, 55, 40]
-        }
-      ]
+    const getMessageCount = (label) => {
+      let msgCount = 0
+      let msgIndex = dataIndex.props.children
+      msgIndex.forEach(msg => {
+        if (msg.props.children === label){
+          msgCount++
+        } 
+      })
+      return msgCount
     }
+
+    // if (this.state.distanceData != null) {
+    //   distanceIndex = (
+    //     <div>
+    //       {this.state.performanceData.map(item => {
+    //         return <div key={item.id}>{item.data.distance}</div>
+    //       })}
+    //     </div>
+    //   )
+    // }
+
+    const data = {
+      labels: [
+        'Excellent',
+        'Above average',
+        'Average',
+        'Below average',
+        'Poor'
+      ],
+      datasets: [{
+        //data: [30, 5, 10, 20, 12],
+        data: [getMessageCount('Poor'), getMessageCount('Below average'), getMessageCount('Poor'), getMessageCount('Poor'), getMessageCount('Poor')],
+        backgroundColor: [
+        '#3b9977',
+        '#994b3b',
+        '#997a3b',
+        '#593b99',
+        '#995372'
+        ],
+        hoverBackgroundColor: [
+        '#2ad1a4',
+        '#e0512d',
+        '#d4a026',
+        '#a28abd',
+        '#e02d74'
+        ]
+      }]
+    };
 
     return (
       <div>
-        <h2>Line Chart Example</h2>
-        <Line data={mydata} />
+        <h2>Doughnut Example</h2>
+        <Doughnut data={data} />
         {dataIndex}
-        {distanceIndex}
+        {/* {distanceIndex} */}
       </div>
     )
   }
