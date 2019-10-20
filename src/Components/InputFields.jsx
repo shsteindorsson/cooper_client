213824b1db5a4eb07441fc54b7850339e619c4b2
clalil <cliljander@gmail.com>
2019-10-20 08:24:15 +0200

    Add styling to select option, add favicon.

--- src/Components/InputFields.jsx
+++ src/Components/InputFields.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Input } from 'semantic-ui-react'
+import { Select, Input } from 'semantic-ui-react'
 
 const description = [
   'This is a simple fitness tracking application.',
